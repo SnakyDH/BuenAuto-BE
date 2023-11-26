333e8e0767bd3173ee66dabdd8034ca9f0a6e288
SnakyDH <skdanielg5@gmail.com>
2023-11-26 18:46:06 -0500

    feat: config DB using Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDataBaseConfig } from './config/db.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [configuration],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(getDataBaseConfig()),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
