f46d31bac31f310a35d05340faf521f35e566fa6
SnakyDH <skdanielg5@gmail.com>
2023-11-29 22:08:41 -0500

    feat: Swagger

--- src/modules/employee/services/employee.service.ts
+++ src/modules/employee/services/employee.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EmployeeEntity } from '../entities/employee.entity';
+import { Repository } from 'typeorm';
+import { Employee } from '../interfaces/employee';
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+  constructor(
+    @InjectRepository(EmployeeEntity)
+    private readonly repo: Repository<EmployeeEntity>,
+  ) {}
+  async getAll() {
+    return await this.repo.find();
+  }
+  async create(employee: Employee) {
+    console.log(employee);
+    //this.repo.save(employee);
+  }
+  async update() {
+    return {};
+  }
+  async getPositions() {
+    return this.repo.find;
+  }
+  async dismiss() {
+    return {};
+  }
+}
